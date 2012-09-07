8b09043ceae822edf6954f89f1bf7cabe7aff9f6
Björn Hjortsten <bjorn.hjortsten@kaigan.se>
2012-09-07 09:49:46 +0200

    Added more documentation.

--- QBankAccountAPI.php
+++ QBankAccountAPI.php
@@ -2,9 +2,20 @@
 require_once 'QBankAPI.php';
 
 require_once 'model/Group.php';
-	
+
+/**
+ * Provides functionality for accounts (users, groups, etc.) in QBank.
+ * @author Björn Hjortsten
+ * @copyright Kaigan 2012
+ * @package QBankAPIWrapper
+ */
 class QBankAccountAPI extends QBankAPI {
 	
+	/**
+	 * Gets all the Groups the currently logged in user is member of.
+	 * @author Björn Hjortsten
+	 * @return Group[]
+	 */
 	public function getGroups() {
 		$result = $this->call('getgroups', array());
 		$groups = array();
@@ -14,6 +25,12 @@ class QBankAccountAPI extends QBankAPI {
 		return $groups;
 	}
 	
+	/**
+	 * Gets a specific Group.
+	 * @param int $id The id of the Group to get.
+	 * @author Björn Hjortsten
+	 * @return Group
+	 */
 	public function getGroup($id) {
 		$result = $this->call('getgroupinformation', array('groupId' => $id));
 		return Group::createFromRawObject($result->group);
